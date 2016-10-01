75223b9abad29066dd5e192c6faa58469062a23d
ardUbenga <arduino.ubenga@gmail.com>
2016-10-01 23:14:13 +0200

    01/10/2016 23:13

---
+++ medidordistancia/medidor_distacia/medidor_distacia/Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+
+Public Class Form1
+    Shared _continue As Boolean
+    Shared _serialPort As SerialPort
+    Private Sub Form1_Disposed(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Disposed
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        SerialPort1.PortName = "com4"
+        SerialPort1.BaudRate = "9600"
+        SerialPort1.DataBits = "8"
+        SerialPort1.Parity = Parity.None
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.Handshake = Handshake.None
+        SerialPort1.Encoding = System.Text.Encoding.Default
+        SerialPort1.Open()
+        CheckForIllegalCrossThreadCalls = False ' Esta línea no le gusta mucho a los programadores,
+        ' pero aquí la ponemos para que funcione el Label1
+
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim distancia As String
+        distancia = SerialPort1.ReadLine 
+        If distancia < 20 Then
+            TextBox1.ForeColor = Color.Red
+        Else : TextBox1.ForeColor = Color.Black
+        End If
+        TextBox1.Text = distancia
+
+    End Sub
+End Class
---
+++ medidordistancia/medidordistancia.ino
@@ -0,0 +1,55 @@
+int Trig=8;
+int Echo=10;
+ 
+//Variable en la que se va a almacenar el valor correspondiente a la distancia
+int Dist; 
+void setup() {
+ 
+pinMode (Trig, OUTPUT);
+pinMode (Echo, INPUT);
+Serial.begin(9600); 
+}
+ //Este módulo calcula y devuelve la distancia en cm.
+/*
+Puedes poner el código del módulo directamente en el loop o utilizar el módulo
+para reducir el número de líneas de código del loop o reutilizar el código
+*/
+int ultrasonido (){
+ 
+//Para estabilizar el valor del pin Trig se establece a LOW
+digitalWrite (Trig, LOW);
+delay(10);
+ 
+//Se lanzan los 8 pulsos
+digitalWrite (Trig, HIGH);
+delay(1);
+digitalWrite (Trig, LOW);
+ 
+/*
+Se mide el tiempo que tarda la señal en regresar y se calcula la distancia.
+ 
+Observa que al realizar pulseIn el valor que se obtiene es tiempo, no distancia
+ 
+Se está reutilizando la variable Distancia.
+*/
+ 
+unsigned long distancia= pulseIn (Echo, HIGH);
+distancia=distancia/58;
+return distancia;
+ 
+delay(100);
+  
+}
+ 
+void loop() {
+int centimetros=ultrasonido ();
+//Serial.print("Distancia: ");
+Serial.println(centimetros);
+//Serial.println(" Cm");
+if(centimetros <= 10)
+  {
+     Serial.println("Que se choca");
+  }
+ 
+delay (1000);
+}
